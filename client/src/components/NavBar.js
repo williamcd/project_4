dd42b0ac9b5f7e14df58d67038b9b304f4b1943b
williamcd <wcdetrick@me.com>
2018-04-09 19:49:19 -0400

    cleaned code, edits to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
     render() {
         return (
             <div>
-                <button>HOME</button>
+                <Link to="/shops">
+                    <button>HOME</button>
+                </Link>
                 <h2>WELCOME TO DUNGEONS & TAVERNS</h2>
             </div>
         );
